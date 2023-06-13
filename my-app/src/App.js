ba52a8db94b984863f3e3acbb3ca7429dddf2842
pallavi-rk <91479190+pallavi-rk@users.noreply.github.com>
2023-06-13 19:32:56 +0530

    boilerplate react setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import Container from './Container';
 function App() {
+
   return (
     <div>
-      
+      <Container/>
     </div>
   )
 }
 
 export default App
+
+
 
